0782fb9c62b563e0c9c0c45ed8310dc19dd0f292
Clodoaldo <cas_maquinas@hotmail.com>
2025-02-03 17:26:36 -0300

    Atualização de imagens e implementação do slider automático

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
-const dropdown = document.querySelector('.dropdown')
-const dropdownContent = document.querySelector('.dropdown-content')
+const dropdown = document.querySelector('.dropdown');
+const dropdownContent = document.querySelector('.dropdown-content');
+const box = document.querySelector(".slider")
+const image = document.querySelector(".slider img")
+
+let conter = 0;
+
+function slider() {
+    conter++;
+
+    if(conter >= 5) {
+       conter = 0; 
+    }
+
+    box.style.transform = `translateX(${-conter*800}px)`;
+}
+
+setInterval(slider, 4000);
 
 dropdown.addEventListener('mouseover', () => {
-    dropdownContent.classList.add('show')
-}) 
+    dropdownContent.classList.add('show');
+});
 
 dropdown.addEventListener('mouseout', () => {
-    dropdownContent.classList.remove('show')
-}) 
+    dropdownContent.classList.remove('show');
+});
